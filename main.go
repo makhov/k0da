ba898c6b016d3267cdc3a6e22ab743561575005a
amakhov <amakhov@mirantis.com>
2025-09-10 15:30:19 +0300

    remove commented code

--- main.go
+++ main.go
@@ -13,19 +13,3 @@ func main() {
 		os.Exit(1)
 	}
 }
-
-//podman run -it --rm \
-//--name k0d-node \
-//--hostname k0d-node \
-//--privileged \
-//--tmpfs /run \
-//--tmpfs /var/run \
-//--device /dev/null:/dev/kmsg \
-//--security-opt unmask=ALL \
-//--security-opt seccomp=unconfined \
-//--security-opt apparmor=unconfined \
-//--security-opt label=disable \
-//-v k0d-var:/var \
-//-v /lib/modules:/lib/modules:ro \
-//-p 16443:6443 \
-//quay.io/k0sproject/k0s:v1.33.3-k0s.0 k0s controller --enable-worker
